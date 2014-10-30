9b09c43ef2637c92d75c0e409a957b4df0da55b2
Joe Lafiosca <joe@lafiosca.com>
2014-10-30 01:29:26 -0400

    tweak Github linkback

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('/:ox?/:oy?/:zoom?', function(request, response) {
         result += '<br />';
     }
 
-    result += '<br /><br /><font color="#ffffff" size="-2">Code available at <a href="https://github.com/lafiosca/mandelbrot">Github</a></body></html>';
+    result += '<br /><font color="#ffffff" size="-1">Code available at <a href="https://github.com/lafiosca/mandelbrot"><font color="#ffffff">Github</font></a></body></html>';
 
     response.send(result);
 
